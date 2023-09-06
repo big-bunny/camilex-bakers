f0f010c7de72051a727774cba87ba070ebd5573c
big-bunny <codecenter000@gmail.com>
2023-09-06 20:50:22 +0300

    Initial commit2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // pages/index.tsx
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const products = [
   {
@@ -76,6 +77,22 @@ const products = [
   // Add more products here...
 ];
 
+
+// Links for navigation
+const links = [
+  { href: "#home", text: "Home" },
+  { href: "#products", text: "Products" },
+  { href: "#about", text: "About Us" },
+  { href: "#contact", text: "Contact" },
+];
+
+// Contact details
+const contactDetails = [
+  { label: "Phone", value: "+254-723-269-204 or +254-710-951-902" },
+  { label: "Email", value: "info@camilexbakers.com" },
+  { label: "Address", value: "Pipeline, Nairobi" },
+];
+
 export default function Home() {
   return (
     <div className="bg-purple-200 min-h-screen flex flex-col items-center">
@@ -88,48 +105,70 @@ export default function Home() {
       {/* Header */}
       <header className="bg-purple-800 text-white py-4 w-full">
         <div className="container mx-auto flex justify-between items-center">
-          <h1 className="text-4xl font-bold">Camilex Bakers</h1>
+          <a href="#home" className="text-4xl font-bold">Camilex Bakers</a>
           <nav>
             <ul className="flex space-x-6">
-              <li className="text-xl">Home</li>
-              <li className="text-xl">Products</li>
-              <li className="text-xl">About Us</li>
-              <li className="text-xl">Contact</li>
+              {links.map((link, index) => (
+                <li className="text-xl" key={index}>
+                  <a href={link.href}>{link.text}</a>
+                </li>
+              ))}
             </ul>
           </nav>
         </div>
       </header>
 
-      {/* Main Content */}
-      <main className="container mx-auto mt-8 flex flex-wrap justify-center">
-        {products.map((product, index) => (
-          <div key={index} className="max-w-md bg-white rounded-lg shadow-lg m-4 p-4">
-            <Image
-              src={product.image}
-              alt={product.name}
-              width={300}
-              height={200}
-              className="rounded-t-lg"
-            />
-            <h2 className="text-xl font-semibold text-purple-800 mt-2">{product.name}</h2>
-            <p className="text-gray-600">{product.description}</p>
-            <p className="text-gray-700">
-              <span className="font-semibold">Ingredients: </span>
-              {product.ingredients}
-            </p>
-            <p className="text-purple-600 font-semibold">{product.price}</p>
-          </div>
-        ))}
-      </main>
+      {/* Home Section */}
+      <section id="home" className="container mx-auto mt-8">
+        <h2 className="text-3xl font-semibold text-purple-800">Welcome to Camilex Bakers</h2>
+        <p className="text-gray-600 mt-4">
+          We provide delicious baked goods that will satisfy your cravings.
+        </p>
+      </section>
+
+      {/* Products Section */}
+      <section id="products" className="container mx-auto mt-8">
+        <h2 className="text-3xl font-semibold text-purple-800">Our Products</h2>
+        <main className="container mx-auto mt-8 flex flex-wrap justify-center">
+          {products.map((product, index) => (
+            <div key={index} className="max-w-md bg-white rounded-lg shadow-lg m-4 p-4">
+              <Image
+                src={product.image}
+                alt={product.name}
+                width={300}
+                height={200}
+                className="rounded-t-lg"
+              />
+              <h2 className="text-xl font-semibold text-purple-800 mt-2">{product.name}</h2>
+              <p className="text-gray-600">{product.description}</p>
+              <p className="text-gray-700">
+                <span className="font-semibold">Ingredients: </span>
+                {product.ingredients}
+              </p>
+              <p className="text-purple-600 font-semibold">{product.price}</p>
+            </div>
+          ))}
+        </main>
+      </section>
+
+      {/* About Section */}
+      <section id="about" className="container mx-auto mt-8">
+        <h2 className="text-3xl font-semibold text-purple-800">About Us</h2>
+        <p className="text-gray-600 mt-4">
+          We are a bakery dedicated to providing high-quality baked goods made with the finest ingredients. Our team of skilled bakers is passionate about creating delicious pastries, cakes, bread, and more to satisfy your cravings.
+        </p>
+      </section>
 
       {/* Footer */}
       <footer className="bg-purple-800 text-white py-8 w-full">
         <div className="container mx-auto flex justify-between">
           <div>
             <h2 className="text-xl font-semibold">Contact Us</h2>
-            <p className="text-gray-300">Phone: +254-723-269-204 or +254-710-951-902</p>
-            <p className="text-gray-300">Email: info@camilexbakers.com</p>
-            <p className="text-gray-300">Address: Pipeline, Nairobi</p>
+            {contactDetails.map((detail, index) => (
+              <p className="text-gray-300" key={index}>
+                {detail.label}: {detail.value}
+              </p>
+            ))}
           </div>
           <div>
             <h2 className="text-xl font-semibold">Follow Us</h2>
